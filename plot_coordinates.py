ac04db8cd4b26c6000b85ff75489fd1bf4f397b1
sandelit <thomas.sandelin@gmail.com>
2021-12-07 21:02:14 +0200

    Added raycasting to check if point is within bounds

--- plot_coordinates.py
+++ plot_coordinates.py
@@ -3,14 +3,12 @@ import json
 
 f = open('./data/helsinki_new.json')
 data = json.load(f)
-
-
 coord = data["coordinates"]
 
 ys, xs = zip(*coord) #create lists of x and y values
 
 plt.figure()
 plt.plot(xs,ys)
-plt.show() # if you need...
+plt.show()
 
 f.close()
