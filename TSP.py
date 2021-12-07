374c1dcc7fb094b77f16c9c7e248b46d11c11e19
sandelit <thomas.sandelin@gmail.com>
2021-12-07 22:11:39 +0200

    Added TSP.py

---
+++ TSP.py
@@ -0,0 +1,115 @@
+class TravellingSales:
+
+
+    def __init__(self, coords=None, distances=None):
+
+        if coords is None and distances is None:
+            raise Exception("""At least one of coords and distances must be"""
+                            + """ specified.""")
+
+        elif coords is not None:
+            self.is_coords = True
+            path_list = []
+            dist_list = []
+
+        else:
+            self.is_coords = False
+
+            # Remove any duplicates from list
+            distances = list({tuple(sorted(dist[0:2]) + [dist[2]])
+                              for dist in distances})
+
+            # Split into separate lists
+            node1_list, node2_list, dist_list = zip(*distances)
+
+            if min(dist_list) <= 0:
+                raise Exception("""The distance between each pair of nodes"""
+                                + """ must be greater than 0.""")
+            if min(node1_list + node2_list) < 0:
+                raise Exception("""The minimum node value must be 0.""")
+
+            if not max(node1_list + node2_list) == \
+                    (len(set(node1_list + node2_list)) - 1):
+                raise Exception("""All nodes must appear at least once in"""
+                                + """ distances.""")
+
+            path_list = list(zip(node1_list, node2_list))
+
+        self.coords = coords
+        self.distances = distances
+        self.path_list = path_list
+        self.dist_list = dist_list
+        self.prob_type = 'tsp'
+
+    def evaluate(self, state):
+        """Evaluate the fitness of a state vector.
+        Parameters
+        ----------
+        state: array
+            State array for evaluation. Each integer between 0 and
+            (len(state) - 1), inclusive must appear exactly once in the array.
+        Returns
+        -------
+        fitness: float
+            Value of fitness function. Returns :code:`np.inf` if travel between
+            two consecutive nodes on the tour is not possible.
+        """
+
+        if self.is_coords and len(state) != len(self.coords):
+            raise Exception("""state must have the same length as coords.""")
+
+        if not len(state) == len(set(state)):
+            raise Exception("""Each node must appear exactly once in state.""")
+
+        if min(state) < 0:
+            raise Exception("""All elements of state must be non-negative"""
+                            + """ integers.""")
+
+        if max(state) >= len(state):
+            raise Exception("""All elements of state must be less than"""
+                            + """ len(state).""")
+
+        fitness = 0
+
+        # Calculate length of each leg of journey
+        for i in range(len(state) - 1):
+            node1 = state[i]
+            node2 = state[i + 1]
+
+            if self.is_coords:
+                fitness += np.linalg.norm(np.array(self.coords[node1])
+                                          - np.array(self.coords[node2]))
+            else:
+                path = (min(node1, node2), max(node1, node2))
+
+                if path in self.path_list:
+                    fitness += self.dist_list[self.path_list.index(path)]
+                else:
+                    fitness += np.inf
+
+        # Calculate length of final leg
+        node1 = state[-1]
+        node2 = state[0]
+
+        if self.is_coords:
+            fitness += np.linalg.norm(np.array(self.coords[node1])
+                                      - np.array(self.coords[node2]))
+        else:
+            path = (min(node1, node2), max(node1, node2))
+
+            if path in self.path_list:
+                fitness += self.dist_list[self.path_list.index(path)]
+            else:
+                fitness += np.inf
+
+        return fitness
+
+    def get_prob_type(self):
+        """ Return the problem type.
+        Returns
+        -------
+        self.prob_type: string
+            Specifies problem type as 'discrete', 'continuous', 'tsp'
+            or 'either'.
+        """
+        return self.prob_type
