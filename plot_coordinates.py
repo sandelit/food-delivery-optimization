e815567b4c1923acf44ee4f47e55186817c4651b
sandelit <thomas.sandelin@gmail.com>
2021-12-07 20:07:17 +0200

    undo

--- plot_coordinates.py
+++ plot_coordinates.py
@@ -8,8 +8,7 @@ data = json.load(f)
 coord = data["coordinates"]
 
 ys, xs = zip(*coord) #create lists of x and y values
-# test comment 123
-# test2123 commenta
+
 plt.figure()
 plt.plot(xs,ys)
 plt.show() # if you need...
